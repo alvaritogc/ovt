bc250dc532d64bf7be35a643548a4b480b07b822
lvaldez <valdez.heliberto@gmail.com>
2013-11-01 19:26:19 -0400

    correcciones en declaracion bean

--- src/main/java/bo/gob/mintrabajo/ovt/bean/declaracion/declaracionBean.java
+++ src/main/java/bo/gob/mintrabajo/ovt/bean/declaracion/declaracionBean.java
@@ -241,7 +241,7 @@ public class declaracionBean implements Serializable {
 
     public void obtenerPeriodoLista(){
         parObligacionCalendarioLista = new ArrayList<ParObligacionCalendario>();
-        parObligacionCalendarioLista = iObligacionCalendarioService.obtenerObligacionCalendario();
+        parObligacionCalendarioLista = iObligacionCalendarioService.listaObligacionCalendario();
     }
 
 
