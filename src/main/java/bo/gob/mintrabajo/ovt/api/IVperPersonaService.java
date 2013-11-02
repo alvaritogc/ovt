e8d493c04f27f8f792fc4d13fd4d256c07001b28
gmercado <garymuchopunk@hotmail.com>
2013-11-02 15:07:18 -0400

    implementada la funcionalidad de cargar un objeto a partir de una Vista de Oracle

---
+++ src/main/java/bo/gob/mintrabajo/ovt/api/IVperPersonaService.java
@@ -0,0 +1,17 @@
+package bo.gob.mintrabajo.ovt.api;
+
+import bo.gob.mintrabajo.ovt.entities.VperPersona;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gmercado
+ * Date: 11/1/13
+ * Time: 5:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IVperPersonaService {
+
+    public VperPersona cargaVistaPersona(String id);
+}
