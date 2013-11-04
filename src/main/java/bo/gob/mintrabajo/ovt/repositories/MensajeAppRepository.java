d4b1a7182471208adec420b506e6c47d5764aaef
pc01 <vic531990@gmail.com>
2013-11-04 18:16:32 -0400

    Adicionando contenidos del template

--- src/main/java/bo/gob/mintrabajo/ovt/repositories/MensajeAppRepository.java
+++ src/main/java/bo/gob/mintrabajo/ovt/repositories/MensajeAppRepository.java
@@ -26,4 +26,12 @@ public interface MensajeAppRepository extends OpenJpaRepository<ParMensajeApp, L
             + " and :fecha between m.fechaDesde and m.fechaHasta "
     )
     List<ParMensajeApp> buscarPorRecursoFecha(@Param("etiqueta") String etiqueta,@Param("fecha") Date fecha);
+     
+    @Query(
+            "   select m "
+            + " from ParMensajeApp m"
+            + " where "
+            + " m.idRecurso.idRecurso = :idRecurso "
+    )
+    List<ParMensajeApp> buscarPorRecurso(@Param("idRecurso") Long idRecurso);
 }
