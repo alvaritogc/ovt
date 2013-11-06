2352e4a85f589df4e8d2ad88dcfe1ae024de9700
renato_velasquez <krizalida.ren@gmail.com>
2013-11-05 22:03:17 -0400

    declaracion...

--- src/main/java/bo/gob/mintrabajo/ovt/Util/Dominios.java
+++ src/main/java/bo/gob/mintrabajo/ovt/Util/Dominios.java
@@ -8,27 +8,30 @@ package bo.gob.mintrabajo.ovt.Util;
  * To change this template use File | Settings | File Templates.
  */
 public class Dominios {
+    public static final String DOM_TIPOS_EMPRESA="TEMPRESA";
+    public static final String DOM_TIPOS_SOCIEDAD="TSOCIEDAD";
+    public static final String DOM_TIPOS_IDENTIFICACION="TIDENTIFICACION";
 
-      public static final String DOM_TIPOS_EMPRESA="TEMPRESA";
-      public static final String DOM_TIPOS_SOCIEDAD="TSOCIEDAD";
-      public static final String DOM_TIPOS_IDENTIFICACION="TIDENTIFICACION";
+    public static final String DOM_ESTADO="ESTADO";
+    public static final String PAR_ESTADO_ACTIVO="A";
+    public static final String PAR_ESTADO_INACTIVO="X";
 
-      public static final String DOM_ESTADO="ESTADO";
-      public static final String PAR_ESTADO_ACTIVO="A";
-      public static final String PAR_ESTADO_INACTIVO="X";
+    public static final String DOM_TIPO_AUTENTICACION="TAUTENTICACION";
+    public static final String PAR_TIPO_AUTENTICACION_LOCAL="LOCAL";
+    public static final String PAR_TIPO_AUTENTICACION_LDAP="LDAP";
 
-      public static final String DOM_TIPO_AUTENTICACION="TAUTENTICACION";
-      public static final String PAR_TIPO_AUTENTICACION_LOCAL="LOCAL";
-      public static final String PAR_TIPO_AUTENTICACION_LDAP="LDAP";
+    public static final String DOM_ESTADO_USUARIO="EUSUARIO";
+    public static final String PAR_ESTADO_USUARIO_ACTIVO="A";
+    public static final String PAR_ESTADO_USUARIO_INACTIVO="X";
+    public static final String PAR_ESTADO_USUARIO_BLOQUEADO="B";
+    public static final String PAR_ESTADO__USUARIO_DEPURADO="D";
 
-      public static final String DOM_ESTADO_USUARIO="EUSUARIO";
-      public static final String PAR_ESTADO_USUARIO_ACTIVO="A";
-      public static final String PAR_ESTADO_USUARIO_INACTIVO="X";
-      public static final String PAR_ESTADO_USUARIO_BLOQUEADO="B";
-      public static final String PAR_ESTADO__USUARIO_DEPURADO="D";
+    public static final String DOM_ESTADO_UNIDAD="EUNIDAD";
+    public static final String PAR_ESTADO_UNIDAD_BAJA_DEFINITIVA="0";
+    public static final String PAR_ESTADO_UNIDAD_ACTIVO="1";
+    public static final String PAR_ESTADO_UNIDAD_INACTIVO="2";
 
-      public static final String DOM_ESTADO_UNIDAD="EUNIDAD";
-      public static final String PAR_ESTADO_UNIDAD_BAJA_DEFINITIVA="0";
-      public static final String PAR_ESTADO_UNIDAD_ACTIVO="1";
-      public static final String PAR_ESTADO_UNIDAD_INACTIVO="2";
+    public static final String EXTENSION_XLS="XLS";
+    public static final String EXTENSION_XLSX="XLSX";
+    public static final String EXTENSION_CSV="CSV";
 }
--- src/main/java/bo/gob/mintrabajo/ovt/bean/declaracion/declaracionBean.java
+++ src/main/java/bo/gob/mintrabajo/ovt/bean/declaracion/declaracionBean.java
@@ -76,7 +76,6 @@ public class declaracionBean implements Serializable {
     private Long numeroOrden;
 
     private String fechaTexto;
-    private String temporal;
     private boolean temporalBoolean;
     private PerPersona persona;
     private Date fechaTemp = new Date();
@@ -94,6 +93,8 @@ public class declaracionBean implements Serializable {
     private boolean estaDeclarado;
     private Long idEntidadSalud;
 
+    private Long idRectificatorio;
+
     @PostConstruct
     public void ini() {
         try {
@@ -102,12 +103,14 @@ public class declaracionBean implements Serializable {
             if (((HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest()).getParameter("p") != null|| !((HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest()).getParameter("p").equals(""))
                 parametro = Integer.valueOf(((HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest()).getParameter("p"));
         }catch (Exception e) {
-            e.printStackTrace();
+//            e.printStackTrace();
             parametro = 1;
         }
 
         if (parametro==3)
             esRectificatorio=true;
+        if(parametro!=1)
+            habilita=false;
 
         idPersona = (String) session.getAttribute("idEmpleador");
         logger.info("buscando persona:"+idPersona);
@@ -241,6 +244,9 @@ public class declaracionBean implements Serializable {
             logger.info(listaBinarios.toString());
             logger.info(docPlanilla.toString());
             generaPlanilla();
+
+//            valida(listaBinarios);
+
             idDocumentoService.guardaDocumentoPlanillaBinario(documento, docPlanilla, listaBinarios);
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Información", "Guardado correctamente"));
             return "irEscritorio";
@@ -267,6 +273,263 @@ public class declaracionBean implements Serializable {
         parObligacionCalendarioLista = iObligacionCalendarioService.listaObligacionCalendario();
     }
 
+//    public void validaArchivo(){
+//
+//
+//
+//        try {
+//            for(DocBinario docBinario:listaBinarios){
+//                CsvReader csvReader;
+////                if(!FilenameUtils.getExtension(docBinario.getTipoDocumento()).toUpperCase().equals(Dominios.EXTENSION_CSV)){
+////                    OutputStream output= XlsToCSV.xlsToCsv(new ByteArrayInputStream(docBinario.getBinario()));
+////                    csvReader = new CsvReader(new InputStreamReader(new ByteArrayInputStream(((ByteArrayOutputStream) output).toByteArray())));
+////                }
+////                else
+////                    csvReader = new CsvReader(new InputStreamReader(new ByteArrayInputStream(docBinario.getBinario())));
+//
+//                csvReader= new CsvReader("/home/rvelasquez/Desktop/planillaComercialPrueba.csv");
+//
+//                csvReader.readRecord();
+//                List<String> columnasTitulo =new ArrayList<String>();
+//                for (int i=0;i<csvReader.getColumnCount();i++)
+//                    columnasTitulo.add(csvReader.get(i));
+//
+//
+//
+//
+//
+//
+//                while (csvReader.readRecord()) {
+//
+//                }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//            }
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
+
+
+
+
+
+
+
+
+
+
+//    public boolean valida(List<DocBinario> listaBinarios){
+//        List<String> errores = new ArrayList<String>();
+//        String errorFilas = "";
+//        String errorCampos = "";
+//        String mensaje;
+//        List <DocPlanillaDetalle>docPlanillaDetalleList = new ArrayList<DocPlanillaDetalle>();
+//        boolean verifica =false;
+//        try {
+//            for(DocBinario docBinario:listaBinarios){
+//                CsvReader csvReader;
+//                if(!FilenameUtils.getExtension(docBinario.getTipoDocumento()).toUpperCase().equals(Dominios.EXTENSION_CSV)){
+//                    OutputStream output= XlsToCSV.xlsToCsv(new ByteArrayInputStream(docBinario.getBinario()));
+//                    csvReader = new CsvReader(new InputStreamReader(new ByteArrayInputStream(((ByteArrayOutputStream) output).toByteArray())));
+//                }
+//                else
+//                    csvReader = new CsvReader(new InputStreamReader(new ByteArrayInputStream(docBinario.getBinario())));
+//
+//
+//                int fila = 0;
+//                csvReader.readRecord();
+//                while (csvReader.readRecord()) {
+//                    fila++;
+//
+//                    int columnCount = csvReader.getColumnCount();
+//                    if (columnCount != 33) {
+//                        errorFilas = "La fila " + fila + ", tiene " + columnCount + " columnas, esto no es aceptado como un documento válido: o el SEPARADOR = " + csvReader.getDelimiter() + " No es el adecuado ";
+//                        errorFilas = errorFilas + fila + ", ";
+//                        continue;
+//                    }
+//
+//                    DocPlanillaDetalle docPlanillaDetalle = new DocPlanillaDetalle();
+//
+////                    docPlanillaDetalle.setIdPlanilla(docPlanilla);
+////                    docPlanillaDetalle.setIdPlanillaDetalle(utils.valorSecuencia("DOC_PLANILLA_DETALLE_SEC"));
+//
+//
+//                    docPlanillaDetalle.setTipoDocumento(csvReader.get(1));
+//
+//                    if (!UtilityData.isInteger(csvReader.get(2)))
+//                        errorCampos = errorCampos + "Número de documento de identidad no es un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setNumeroDocumento(csvReader.get(2));
+//
+//                    docPlanillaDetalle.setExtencionDocumento(csvReader.get(3));
+//                    docPlanillaDetalle.setAfp(csvReader.get(4));
+//                    docPlanillaDetalle.setNuaCua(csvReader.get(5));
+//                    docPlanillaDetalle.setNombre(csvReader.get(6)+" "+csvReader.get(7)+" "+csvReader.get(8)+" "+csvReader.get(9)+" "+csvReader.get(10));
+//                    docPlanillaDetalle.setNacionalidad(csvReader.get(11));
+//                    docPlanillaDetalle.setFechaNacimiento(csvReader.get(12));
+//                    docPlanillaDetalle.setSexo(csvReader.get(13));
+//                    docPlanillaDetalle.setJubilado(csvReader.get(14));
+//                    docPlanillaDetalle.setClasificacionLaboral(csvReader.get(15));
+//                    docPlanillaDetalle.setCargo(csvReader.get(16));
+//                    docPlanillaDetalle.setFechaIngreso(csvReader.get(17));
+//
+//                    if (!UtilityData.isInteger(csvReader.get(18))&&!csvReader.get(18).equals(""))
+//                        errorCampos = errorCampos + "Horas pagadas (día) debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setHorasPagadasDia(csvReader.get(18));
+//
+//                    if (!UtilityData.isInteger(csvReader.get(19)))
+//                        errorCampos = errorCampos + "Días haber básico debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setDiasHaberBasico(csvReader.get(19));
+//
+//                    if (!UtilityData.isInteger(csvReader.get(20)))
+//                        errorCampos = errorCampos + "Días pagados (mes) debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setDiasPagados(csvReader.get(20));
+//
+//                    if (!UtilityData.isInteger(csvReader.get(21))&&!csvReader.get(21).equals(""))
+//                        errorCampos = errorCampos + "Nº de dominicales debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setDominicalMes(csvReader.get(21));
+//
+//                    if (!UtilityData.isDecimal(csvReader.get(22))&&!csvReader.get(22).equals(""))
+//                        errorCampos = errorCampos + "Domingos trabajados debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setDominicalTrabajado(csvReader.get(22));
+//
+//                    if (!UtilityData.isInteger(csvReader.get(23))&&!csvReader.get(23).equals(""))
+//                        errorCampos = errorCampos + "Horas extra debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setHorasExtra(csvReader.get(23));
+//
+//                    if (!UtilityData.isInteger(csvReader.get(24))&&!csvReader.get(24).equals(""))
+//                        errorCampos = errorCampos + "Horas de recargo nocturno debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setHorasNocturno(csvReader.get(24));
+//
+//                    if (!UtilityData.isInteger(csvReader.get(25))&&!csvReader.get(25).equals(""))
+//                        errorCampos = errorCampos + "Horas extra dominicales debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setHorasDominicales(csvReader.get(25));
+//
+//                    if (!UtilityData.isDecimal(csvReader.get(26))&&!csvReader.get(26).equals(""))
+//                        errorCampos = errorCampos + "Haber básico debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setHaberBasico(csvReader.get(26));
+//
+//                    if (!UtilityData.isDecimal(csvReader.get(27))&&!csvReader.get(27).equals(""))
+//                        errorCampos = errorCampos + "Salario dominical debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setHaberBasico(csvReader.get(27));
+//
+//                    if (!UtilityData.isDecimal(csvReader.get(28))&&!csvReader.get(28).equals(""))
+//                        errorCampos = errorCampos + "Monto pagado por domingo trabajado debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setHaberBasico(csvReader.get(28));
+//
+//                    if (!UtilityData.isDecimal(csvReader.get(29)))
+//                        errorCampos = errorCampos + "Monto pagado por horas extra debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setMontoHorasExtra(csvReader.get(29));
+//
+//                    if (!UtilityData.isDecimal(csvReader.get(30))&&!csvReader.get(30).equals(""))
+//                        errorCampos = errorCampos + "Monto pagado por horas nocturnas debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setHaberBasico(csvReader.get(30));
+//
+//                    if (!UtilityData.isDecimal(csvReader.get(31))&&!csvReader.get(31).equals(""))
+//                        errorCampos = errorCampos + "Monto pagado por horas extra dominicales debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setHaberBasico(csvReader.get(31));
+//
+//                    if (!UtilityData.isDecimal(csvReader.get(32)))
+//                        errorCampos = errorCampos + "Bono de antigüedad debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setBonoAntiguedad(csvReader.get(32));
+//
+//                    if (!UtilityData.isDecimal(csvReader.get(33))&&!csvReader.get(33).equals(""))
+//                        errorCampos = errorCampos + "Bono de producción debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setBonoProduccion(csvReader.get(33));
+//
+//                    if (!UtilityData.isDecimal(csvReader.get(34))&&!csvReader.get(34).equals(""))
+//                        errorCampos = errorCampos + "Subsidio de frontera debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setBonoFrontera(csvReader.get(34));
+//
+//                    if (!UtilityData.isDecimal(csvReader.get(35)))
+//                        errorCampos = errorCampos + "Otros bonos o pagos debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setBonoOtros(csvReader.get(35));
+//
+//                    if (!UtilityData.isDecimal(csvReader.get(36)))
+//                        errorCampos = errorCampos + "Total ganado debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setTotalGanado(csvReader.get(36));
+//
+//                    if (!UtilityData.isDecimal(csvReader.get(37)))
+//                        errorCampos = errorCampos + "Aporte a las AFPs debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setDescuentoAfp(csvReader.get(37));
+//
+//                    if (!UtilityData.isDecimal(csvReader.get(38)))
+//                        errorCampos = errorCampos + "RC-IVA debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setDescuentoRciva(csvReader.get(38));
+//
+//                    if (!UtilityData.isDecimal(csvReader.get(39))&&!csvReader.get(39).equals(""))
+//                        errorCampos = errorCampos + "Otros descuentos debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setDescuentoOtro(csvReader.get(39));
+//
+//                    if (!UtilityData.isDecimal(csvReader.get(40)))
+//                        errorCampos = errorCampos + "Total descuentos debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setDescuentosTotal(csvReader.get(40));
+//
+//                    if (!UtilityData.isDecimal(csvReader.get(41)))
+//                        errorCampos = errorCampos + "Líquido pagable debe ser un número válido, ";
+//                    else
+//                        docPlanillaDetalle.setLiquidoPagable(csvReader.get(41));
+//
+//                    docPlanillaDetalleList.add(docPlanillaDetalle);
+//
+//                    if (errorCampos.length() > 0) {
+//                        mensaje = "en la fila " + (fila) + ": " + errorCampos;
+//                        errores.add(mensaje);
+//                        errorCampos = "";
+//                    }
+//                }
+//            }
+//            if (!errorFilas.equals("")) {
+//                mensaje = "Las siguientes filas: " + errorFilas + " no tienen la cantidad de campos requerido";
+//                errores.add(0, mensaje);
+//            }else{
+//                if(errores.size()>0){
+//                    for(String error:errores)
+//                        System.out.println(error);
+//                }
+//
+//            }
+//        } catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        return verifica;
+//    }
 
     //** Obtenemos todos las entidades de la tabla ENTIDAD **//
     public void obtenerEntidad() {
@@ -522,13 +785,6 @@ public class declaracionBean implements Serializable {
         this.nombres = nombres;
     }
 
-    public String getTemporal() {
-        return temporal;
-    }
-
-    public void setTemporal(String temporal) {
-        this.temporal = temporal;
-    }
     public List<DocDocumento> getDocDocumentoList() {
         return docDocumentoList;
     }
@@ -575,5 +831,13 @@ public class declaracionBean implements Serializable {
 
     public void setIdEntidadSalud(Long idEntidadSalud) {
         this.idEntidadSalud = idEntidadSalud;
+    }
+
+    public Long getIdRectificatorio() {
+        return idRectificatorio;
+    }
+
+    public void setIdRectificatorio(Long idRectificatorio) {
+        this.idRectificatorio = idRectificatorio;
     }
 }
