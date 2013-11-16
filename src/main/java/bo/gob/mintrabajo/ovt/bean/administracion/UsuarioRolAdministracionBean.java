9660b86da2333344df5204a95c99f5cc3571b815
gmercado <garymuchopunk@hotmail.com>
2013-11-16 14:57:55 -0400

    Completada la funcionalidad de administracion de usuario recurso

--- src/main/java/bo/gob/mintrabajo/ovt/bean/administracion/UsuarioRolAdministracionBean.java
+++ src/main/java/bo/gob/mintrabajo/ovt/bean/administracion/UsuarioRolAdministracionBean.java
@@ -1,10 +1,8 @@
 package bo.gob.mintrabajo.ovt.bean.administracion;
 
-import bo.gob.mintrabajo.ovt.api.IPersonaService;
-import bo.gob.mintrabajo.ovt.api.IRolService;
-import bo.gob.mintrabajo.ovt.api.IUsuarioRolService;
-import bo.gob.mintrabajo.ovt.api.IUsuarioService;
+import bo.gob.mintrabajo.ovt.api.*;
 import bo.gob.mintrabajo.ovt.entities.*;
+import org.primefaces.context.RequestContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,6 +30,9 @@ public class UsuarioRolAdministracionBean {
     @ManagedProperty(value="#{rolService}")
     private IRolService iRolService;
 
+    @ManagedProperty(value="#{recursoService}")
+    private IRecursoService iRecursoService;
+
     @ManagedProperty(value="#{usuarioRolService}")
     private IUsuarioRolService iUsuarioRolService;
 
@@ -41,16 +42,35 @@ public class UsuarioRolAdministracionBean {
     @ManagedProperty(value="#{personaService}")
     private IPersonaService iPersonaService;
 
+    @ManagedProperty(value="#{usuarioRecursoService}")
+    private IUsuarioRecursoService iUsuarioRecursoService;
+
     private Boolean seleccionado;
     private Long usuarioSelected;
+    private UsrRecurso recursoSelected;
+    private String tipoPermiso;
+    private Short denegado;
 
+    private Date fechaExpiracion;
+    private Date fechaActual;
     private List<UsrRol> rolLista;
     private List<PerPersona> seleccionadoLista;
+    private List<UsrRecurso> recursoLista;
+    private List<UsrRecurso> usuarioRecursoLista;
 
     private static final Logger log = LoggerFactory.getLogger(UsuarioRolAdministracionBean.class);
 
-    public void cargarRoles(){
+    @PostConstruct
+    public void ini(){
+        usuarioRecursoLista = new ArrayList<UsrRecurso>();
+        //recursoLista = iRecursoService.obtenerTodosRecursoLista();
+    }
+
+    public void buscarRolesPorUsuario(){
         seleccionadoLista = new ArrayList<PerPersona>();
+        recursoLista = iRecursoService.buscarPorUsuario(usuarioSelected);
+        cargarRecursoTemporales();
+
         rolLista = iRolService.getAllRoles();
         for(UsrRol ur : rolLista){
             UsrUsuarioRolPK llave = new UsrUsuarioRolPK();
@@ -69,6 +89,14 @@ public class UsuarioRolAdministracionBean {
         }
     }
 
+    public void cargarRecursoTemporales(){
+        try{
+            usuarioRecursoLista = iRecursoService.obtenerRecursoEnUsuarioRecurso(usuarioSelected);
+        }catch (Exception e){
+            log.info("Error de la api !!!!!!!!!!");
+        }
+    }
+
     public void guardarUsuarioRol() {
         log.info("Ingresando a la clase " + getClass().getSimpleName() + " metodo guardarUsuarioRol()");
         try{
@@ -79,13 +107,77 @@ public class UsuarioRolAdministracionBean {
                 iPersonaService.eliminarUsuarioRol(usuarioSelected, rolLista.get(i).getIdRol());
             }
         }
-        cargarRoles();
-        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Información", "Asignación de roles ejecutado correctamente"));
+        buscarRolesPorUsuario();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Información", "Asignación de roles ejecutado correctamente"));
         }catch (Exception e){
             e.printStackTrace();
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Atención!", "No se pudo editar la asignació de roles!"));
         }
     }
+
+    public void asignarRecursoTemporal(){
+        log.info("Ingresando a la clase " + getClass().getSimpleName() + " metodo asignarRecursoTemporal()");
+        log.info("Asignando el recurso temporal " + recursoSelected.getDescripcion() + " Expira el " + fechaExpiracion);
+
+        if(fechaExpiracion == null){
+            fechaExpiracion = new Date();
+        }
+        UsrUsuarioRecurso ur = new UsrUsuarioRecurso();
+        ur.setFechaLimite(fechaExpiracion);
+        ur.setUsrRecurso(recursoSelected);
+        ur.setWx(tipoPermiso);
+        iUsuarioRecursoService.save(ur, usuarioSelected);
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Información", "Recurso asignado correctamente"));
+        fechaExpiracion = null;
+        buscarRolesPorUsuario();
+        RequestContext context = RequestContext.getCurrentInstance();
+        context.execute("asignaDlg.hide();");
+    }
+
+    public void editarUsuarioRecurso() {
+        log.info("Ingresando a la clase " + getClass().getSimpleName() + " metodo editarUsuarioRecurso()");
+        try {
+            UsrUsuarioRecurso ur = new UsrUsuarioRecurso();
+            ur.setWx(tipoPermiso);
+            ur.setFechaLimite(new java.sql.Date(fechaExpiracion.getTime()));
+            ur.setEsDenegado(denegado);
+
+            UsrUsuarioRecursoPK usrUsuarioRecursoPK = new UsrUsuarioRecursoPK();
+            usrUsuarioRecursoPK.setIdUsuario(usuarioSelected);
+            usrUsuarioRecursoPK.setIdRecurso(recursoSelected.getIdRecurso());
+            iUsuarioRecursoService.editar(ur, usrUsuarioRecursoPK);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Información", "Recurso temporal editado correctamente"));
+            log.info("Editado correctamente ................. ");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void eliminarRecursoTemporal(){
+        log.info("Ingresando a la clase " + getClass().getSimpleName() + " metodo eliminarRecursoTemporal()");
+        UsrUsuarioRecursoPK usrUsuarioRecursoPK = new UsrUsuarioRecursoPK();
+        usrUsuarioRecursoPK.setIdUsuario(usuarioSelected);
+        usrUsuarioRecursoPK.setIdRecurso(recursoSelected.getIdRecurso());
+        boolean tmp = iUsuarioRecursoService.eliminarUsuarioRecurso(usrUsuarioRecursoPK);
+        if(tmp){
+            cargarRecursoTemporales();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Información", "Se quitó el recurso temporal " + recursoSelected.getDescripcion()));
+        }else{
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Atención", "No se pudo quitar el recurso!"));
+        }
+    }
+
+    public void cargarUsuarioRecursoData(){
+        UsrUsuarioRecursoPK usrUsuarioRecursoPK = new UsrUsuarioRecursoPK();
+        usrUsuarioRecursoPK.setIdUsuario(usuarioSelected);
+        usrUsuarioRecursoPK.setIdRecurso(recursoSelected.getIdRecurso());
+        UsrUsuarioRecurso ur = iUsuarioRecursoService.buscarUnUsuarioRecurso(usrUsuarioRecursoPK);
+        tipoPermiso = ur.getWx();
+        fechaExpiracion = ur.getFechaLimite();
+        denegado = ur.getEsDenegado();
+
+    }
+
 
     public List<UsrUsuario> complete(String usuarioLogin){
         try{
@@ -159,5 +251,77 @@ public class UsuarioRolAdministracionBean {
 
     public void setiPersonaService(IPersonaService iPersonaService) {
         this.iPersonaService = iPersonaService;
+    }
+
+    public IRecursoService getiRecursoService() {
+        return iRecursoService;
+    }
+
+    public void setiRecursoService(IRecursoService iRecursoService) {
+        this.iRecursoService = iRecursoService;
+    }
+
+    public IUsuarioRecursoService getiUsuarioRecursoService() {
+        return iUsuarioRecursoService;
+    }
+
+    public void setiUsuarioRecursoService(IUsuarioRecursoService iUsuarioRecursoService) {
+        this.iUsuarioRecursoService = iUsuarioRecursoService;
+    }
+
+    public List<UsrRecurso> getRecursoLista() {
+        return recursoLista;
+    }
+
+    public void setRecursoLista(List<UsrRecurso> recursoLista) {
+        this.recursoLista = recursoLista;
+    }
+
+    public List<UsrRecurso> getUsuarioRecursoLista() {
+        return usuarioRecursoLista;
+    }
+
+    public void setUsuarioRecursoLista(List<UsrRecurso> usuarioRecursoLista) {
+        this.usuarioRecursoLista = usuarioRecursoLista;
+    }
+
+    public Date getFechaExpiracion() {
+        return fechaExpiracion;
+    }
+
+    public void setFechaExpiracion(Date fechaExpiracion) {
+        this.fechaExpiracion = fechaExpiracion;
+    }
+
+    public Date getFechaActual() {
+        return fechaActual = new Date();
+    }
+
+    public void setFechaActual(Date fechaActual) {
+        this.fechaActual = fechaActual;
+    }
+
+    public UsrRecurso getRecursoSelected() {
+        return recursoSelected;
+    }
+
+    public void setRecursoSelected(UsrRecurso recursoSelected) {
+        this.recursoSelected = recursoSelected;
+    }
+
+    public String getTipoPermiso() {
+        return tipoPermiso;
+    }
+
+    public void setTipoPermiso(String tipoPermiso) {
+        this.tipoPermiso = tipoPermiso;
+    }
+
+    public Short getDenegado() {
+        return denegado;
+    }
+
+    public void setDenegado(Short denegado) {
+        this.denegado = denegado;
     }
 }
