bdb2a2a583fea30a0322d521c3e95a2dff2d5af5
renato_velasquez <krizalida.ren@gmail.com>
2013-11-19 17:36:56 -0400

    corrigiendo llamada a query...

--- src/main/java/bo/gob/mintrabajo/ovt/services/ObligacionCalendarioService.java
+++ src/main/java/bo/gob/mintrabajo/ovt/services/ObligacionCalendarioService.java
@@ -30,13 +30,10 @@ public class ObligacionCalendarioService implements IObligacionCalendarioService
     private final ObligacionCalendarioRepository obligacionCalendarioRepository;
     private final CalendarioRepository calendarioRepository;
     private final ObligacionRepository obligacionRepository;
-    
-    
+
+
     @PersistenceContext(unitName = "entityManagerFactory")
     private EntityManager entityManager;
-    
-    
-    
 
     @Inject
     public ObligacionCalendarioService(ObligacionCalendarioRepository obligacionCalendarioRepository,CalendarioRepository calendarioRepository, ObligacionRepository obligacionRepository) {
@@ -44,7 +41,7 @@ public class ObligacionCalendarioService implements IObligacionCalendarioService
         this.calendarioRepository=calendarioRepository;
         this.obligacionRepository=obligacionRepository;
     }
-    
+
     @Override
     public List<ParObligacionCalendario> listaObligacionCalendario(){
         List<ParObligacionCalendario> lista;
@@ -56,7 +53,7 @@ public class ObligacionCalendarioService implements IObligacionCalendarioService
         }
         return lista;
     }
-    
+
     @Override
     public List<ParObligacionCalendario> listaObligacionCalendarioPorObligacion(String codObligacion){
         List<ParObligacionCalendario> lista;
@@ -68,13 +65,13 @@ public class ObligacionCalendarioService implements IObligacionCalendarioService
         }
         return lista;
     }
-    
+
     @Override
-    public ParObligacionCalendario saveObligacionCalendario(ParObligacionCalendario obligacionCalendario, 
-        String gestion, String periodo,String REGISTRO_BITACORA, String parObligacion, boolean evento){               
+    public ParObligacionCalendario saveObligacionCalendario(ParObligacionCalendario obligacionCalendario,
+                                                            String gestion, String periodo,String REGISTRO_BITACORA, String parObligacion, boolean evento){
         ParObligacionCalendario poc=new ParObligacionCalendario();
-        ParCalendario pc=new ParCalendario();      
-        
+        ParCalendario pc=new ParCalendario();
+
         if(obligacionCalendario.getIdObligacionCalendario()==null && !evento){
             poc.setIdObligacionCalendario(this.valorSecuencia("PAR_OBLIGACION_CAL_SEC"));
         }else{
@@ -84,15 +81,15 @@ public class ObligacionCalendarioService implements IObligacionCalendarioService
         poc.setParCalendario(pc);
         poc.setCodObligacion(obligacionRepository.findOne(parObligacion));
         poc.setTipoCalendario(obligacionCalendario.getTipoCalendario());
-           
+
         poc.setFechaDesde(obligacionCalendario.getFechaDesde());
         poc.setFechaHasta(obligacionCalendario.getFechaHasta());
         poc.setFechaPlazo(obligacionCalendario.getFechaPlazo());
-        
+
         poc.setFechaBitacora(new Date());
         poc.setRegistroBitacora(REGISTRO_BITACORA);
 
-        try {    
+        try {
             obligacionCalendario = obligacionCalendarioRepository.save(poc);
         } catch (Exception e) {
             e.printStackTrace();
@@ -100,7 +97,7 @@ public class ObligacionCalendarioService implements IObligacionCalendarioService
         }
         return obligacionCalendario;
     }
-    
+
     @Override
     public boolean deleteObligacionCalendario(ParObligacionCalendario obligacionCalendario){
         boolean deleted = false;
@@ -121,7 +118,7 @@ public class ObligacionCalendarioService implements IObligacionCalendarioService
         List<ParObligacionCalendario> tmpLista = obligacionCalendarioRepository.buscarPorFecha(new Timestamp(new Date().getTime()));
         return tmpLista;
     }
-    
+
     @Override
     public List<ParObligacionCalendario> listaObligacionCalendarioOrdenadoPorDescripcionDeObligacion(){
         List<ParObligacionCalendario> lista;
@@ -138,7 +135,7 @@ public class ObligacionCalendarioService implements IObligacionCalendarioService
         rtn = (BigDecimal)entityManager.createNativeQuery("SELECT "+nombreSecuencia+".nextval FROM DUAL").getSingleResult();
         return rtn.longValue();
     }
-    
+
     @Override
     public List<ParObligacionCalendario> listaObligacionCalendarioPorGestion(String gestionActual){
         List<ParObligacionCalendario> lista;
@@ -150,7 +147,7 @@ public class ObligacionCalendarioService implements IObligacionCalendarioService
         }
         return lista;
     }
-    
+
     @Override
     public ParObligacionCalendario findById(Long id){
         return obligacionCalendarioRepository.findOne(id);
