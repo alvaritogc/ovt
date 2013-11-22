14dd06de0e3e71df38167afdf8759c782aa8d7cd
lvaldez <valdez.heliberto@gmail.com>
2013-11-22 17:22:11 -0400

    modificando codigo y vista

--- src/main/java/bo/gob/mintrabajo/ovt/repositories/DefinicionRepository.java
+++ src/main/java/bo/gob/mintrabajo/ovt/repositories/DefinicionRepository.java
@@ -7,10 +7,22 @@ import name.marcelomorales.siqisiqi.openjpa.spring.OpenJpaSettings;
 
 import java.util.Date;
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @OpenJpaSettings
 public interface DefinicionRepository extends OpenJpaRepository<DocDefinicion, DocDefinicionPK> {
 
     List<DocDefinicion> findByNombreAndFechaBitacora(String nombre, Date fechaBitacora);
+    @Query("select p "
+            + " from DocDefinicion p "
+            + " where p.docDefinicionPK.codDocumento = :codDocumento")
+    List<DocDefinicion> listaVersionesPorCodDocumento(@Param("codDocumento")String codDocumento);
+    
+    @Query("select p "
+            + " from DocDefinicion p "
+            + " where p.docDefinicionPK.codDocumento = :codDocumento and "
+            + " p.docDefinicionPK.version = :version")
+    DocDefinicion obtenerDocDefinicion(@Param("codDocumento")String codDocumento, @Param("version")short version);
 
 }
