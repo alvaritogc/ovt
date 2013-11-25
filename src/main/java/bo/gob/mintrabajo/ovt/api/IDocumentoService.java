bcb4e201b022b493530f3632ce6f433da3eac405
renato_velasquez <krizalida.ren@gmail.com>
2013-11-25 12:10:10 -0400

    generando el reporte ROE PROVISIONALMENTE, correcamente...

--- src/main/java/bo/gob/mintrabajo/ovt/api/IDocumentoService.java
+++ src/main/java/bo/gob/mintrabajo/ovt/api/IDocumentoService.java
@@ -1,7 +1,10 @@
 package bo.gob.mintrabajo.ovt.api;
 
 import bo.gob.mintrabajo.ovt.entities.*;
+import net.sf.jasperreports.engine.JRException;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 public interface IDocumentoService {
@@ -11,7 +14,7 @@ public interface IDocumentoService {
     void guardaDocumentoPlanillaBinario(DocDocumento docDocumento, DocPlanilla docPlanilla, List<DocBinario> listaBinarios, List<DocPlanillaDetalle> docPlanillaDetalles);
     DocDocumento guardarCambioEstado(DocDocumento documento, ParDocumentoEstado codEstadoFinal,String idUsuario);
     public DocDocumento guardarBajaRoe(DocDocumento docDocumento, DocGenerico docGenerico,String registroBitacora);
-    public DocDocumento guardarImpresionRoe(DocDocumento docDocumento, DocGenerico docGenerico,String registroBitacora, DocDefinicion docDefinicion, VperPersona vperPersona, Long idUsuarioEmpleador);
+    public DocDocumento guardarImpresionRoe(DocDocumento docDocumento, DocGenerico docGenerico,String registroBitacora, DocDefinicion docDefinicion);//, VperPersona vperPersona, Long idUsuarioEmpleador);
     String generaReporteLC1010(DocPlanilla docPlanilla, PerPersona persona , DocDocumento docDocumento, PerUnidad perUnidad, VperPersona vperPersona);
 //    String generaReporteROE(VperPersona vperPersona, String idUsuario, String idPersona);
     public DocDocumento guardarRoeGenerico(PerUnidadPK perUnidadPK ,String registroBitacora);
@@ -19,4 +22,7 @@ public interface IDocumentoService {
     public List<DocDocumento>  listarRoe013(String idPersona,long idUnidad);
 
     public List<DocDocumento>ObtenerRoes(String idPersona,long idUnidad);
+
+
+    String generateReport(String nomArchivo, String jasper, HashMap<String, Object> parametros)throws ClassNotFoundException, IOException, JRException;
 }
