8a5c6d023b31f3f5283b798d5ecda034796353fa
pc01 <vic531990@gmail.com>
2013-11-28 17:33:42 -0400

    cambiando idDocDefinicion

--- src/main/java/bo/gob/mintrabajo/ovt/repositories/DefinicionRepository.java
+++ src/main/java/bo/gob/mintrabajo/ovt/repositories/DefinicionRepository.java
@@ -14,26 +14,34 @@ import java.util.List;
 public interface DefinicionRepository extends OpenJpaRepository<DocDefinicion, DocDefinicionPK> {
 
     List<DocDefinicion> findByNombreAndFechaBitacora(String nombre, Date fechaBitacora);
+
     @Query("select p "
-            + " from DocDefinicion p "
-            + " where p.docDefinicionPK.codDocumento = :codDocumento")
-    List<DocDefinicion> listaVersionesPorCodDocumento(@Param("codDocumento")String codDocumento);
-    
+    + " from DocDefinicion p "
+    + " where p.docDefinicionPK.codDocumento = :codDocumento")
+    List<DocDefinicion> listaVersionesPorCodDocumento(@Param("codDocumento") String codDocumento);
+
     @Query("select p "
-            + " from DocDefinicion p "
-            + " where p.docDefinicionPK.codDocumento = :codDocumento and "
-            + " p.docDefinicionPK.version = :version")
-    DocDefinicion obtenerDocDefinicion(@Param("codDocumento")String codDocumento, @Param("version")short version);
+    + " from DocDefinicion p "
+    + " where p.docDefinicionPK.codDocumento = :codDocumento and "
+    + " p.docDefinicionPK.version = :version")
+    DocDefinicion obtenerDocDefinicion(@Param("codDocumento") String codDocumento, @Param("version") short version);
 
     @Query(
             "   select a "
-                    + " from DocDefinicion a"
-                    + " order by a.nombre asc "
-    )
+    + " from DocDefinicion a"
+    + " order by a.nombre asc ")
     List<DocDefinicion> listaPorOrdenDocDefinicion();
 
     @Query(
-            "select a from DocDefinicion a where (a.docDefinicionPK.codDocumento like :cod and a.docDefinicionPK.version =:ver)"
-    )
-    List<DocDefinicion> listaCodDocumento(@Param("cod") String cod,@Param("ver") Short ver);
+            "select a from DocDefinicion a where (a.docDefinicionPK.codDocumento like :cod and a.docDefinicionPK.version =:ver)")
+    List<DocDefinicion> listaCodDocumento(@Param("cod") String cod, @Param("ver") Short ver);
+
+    @Query(
+            "   select d "
+            + " from DocDefinicion d"
+            + " where"
+            + " d.docDefinicionPK.codDocumento=:codDocumento "
+            + " and d.estado= 'A' "
+            )
+    DocDefinicion buscarPorCodDocumentoActivo(@Param("codDocumento") String codDocumento);
 }
