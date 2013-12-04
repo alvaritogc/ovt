df5ceb446cc719e1afda7cc849e626e307fb0ca2
gmercado <garymuchopunk@hotmail.com>
2013-12-04 15:34:58 -0400

    Quitando test de webservice

--- src/test/java/Service1Test.java
+++ src/test/java/Service1Test.java
@@ -14,22 +14,22 @@ import java.net.URL;
  * Time: 4:25 PM
  * To change this template use File | Settings | File Templates.
  */
-public class Service1Test {
-
-    @Test
-    public void bienvenidaTest() {
-        Service1 s = new Service1();
-        Service1Soap service1Soap = s.getService1Soap();
-        WSSAPWEBPARAMResponse.WSSAPWEBPARAMResult lp = service1Soap.wssapwebparam("4332051", "LP");
-        ElementNSImpl element = (ElementNSImpl) lp.getAny();
-        System.out.println(">>>>"+element.getLocalName());
-
-        ElementNSImpl dataSet = (ElementNSImpl) element.getElementsByTagName("NewDataSet").item(0);
-        if(dataSet!=null){
-            ElementNSImpl consulta = (ElementNSImpl) dataSet.getFirstChild();
-            if(consulta!=null){
-                System.out.println("fun_id>>>"+consulta.getElementsByTagName("fun_id").item(0).getTextContent());
-            }
-        }
-    }
-}
+//public class Service1Test {
+//
+//    @Test
+//    public void bienvenidaTest() {
+//        Service1 s = new Service1();
+//        Service1Soap service1Soap = s.getService1Soap();
+//        WSSAPWEBPARAMResponse.WSSAPWEBPARAMResult lp = service1Soap.wssapwebparam("4332051", "LP");
+//        ElementNSImpl element = (ElementNSImpl) lp.getAny();
+//        System.out.println(">>>>"+element.getLocalName());
+//
+//        ElementNSImpl dataSet = (ElementNSImpl) element.getElementsByTagName("NewDataSet").item(0);
+//        if(dataSet!=null){
+//            ElementNSImpl consulta = (ElementNSImpl) dataSet.getFirstChild();
+//            if(consulta!=null){
+//                System.out.println("fun_id>>>"+consulta.getElementsByTagName("fun_id").item(0).getTextContent());
+//            }
+//        }
+//    }
+//}
